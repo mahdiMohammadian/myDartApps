2cfb47a13cd1bac4591c9327f788494e79b685b9
Mahdi Mohammadian <mahdi786.mmm@gmail.com>
2021-11-16 21:46:06 +0430

    commit first

--- tryCatch.dart
+++ tryCatch.dart
@@ -1,19 +1,14 @@
-
-import 'dart:io';
 import 'dart:math';
-
+import 'getUserInput.dart';
 void main(List<String> args) {
  Random rand = Random();
    int easyG= rand.nextInt(10);
    int mediumG= rand.nextInt(50);
    int hardG= rand.nextInt(100);
-
-   stdout.write("please select a level 1: for easy, \n 2: for medium \n 3: for dificult");
-   int level = stdin.readLineSync() as int;
+   int level = getUserInput("please select a level \n 1: for easy \n 2: for medium \n 3: for dificult\n level ");
    if(level ==1)
    {
-     stdout.write("please guess a number : ");
-     double? num = double.tryParse(stdin.readLineSync());
+     int num = getUserInput("please guess a number between 1 and 10 ");
      if(num==easyG)
      {
        print(" you won the game the number is correct ");
@@ -23,8 +18,7 @@ void main(List<String> args) {
      }
    } else if (level==2)
    {
-     stdout.write("please guess a number : ");
-     int num = stdin.readLineSync() as int;
+     int num = getUserInput("please guess a number between 1 and 50 ");
      if(num==mediumG)
      {
        print(" you won the game the number is correct");
@@ -32,11 +26,9 @@ void main(List<String> args) {
      else {
        print(" wrong number ");
      }
-   } else
+   } else if(num==3)
    {
-     
-     stdout.write("please guess a number : ");
-     int num = stdin.readLineSync() as int;
+     int num = getUserInput("please guess a number between 1 and 100 ");
      if(num==hardG)
      {
        print(" you won the game the number is correct");
@@ -45,8 +37,7 @@ void main(List<String> args) {
        print(" wrong number ");
      }
    }
-   
-
+    else {
+     print(" you've selected the wrong level ");
+   }
  }
-
- 
